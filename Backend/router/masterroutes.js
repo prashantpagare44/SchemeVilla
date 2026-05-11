5c416a7e84bd96f5f0dbe19e9581a76f2e6237e4
prashantpagare44 <prashantpagarejd@gmail.com>
2026-05-11 17:11:34 +0530

    masterroute changes

--- Backend/router/masterroutes.js
+++ Backend/router/masterroutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { CreateZone , getZone , Createcompany , getCompany} from '../controller/masterdatacontroller.js';
+import { CreateZone , getZone , Createcompany , getCompany , UpdateZone , DeleteZone , UpdateCompany , DeleteCompany } from '../controller/masterdatacontroller.js';
 import { protect } from '../middleware/authMiddleware.js';
 import { authorizeRoles } from '../middleware/authMiddleware.js';
 const router = express.Router();
@@ -9,6 +9,10 @@ router.post('/zone', protect, authorizeRoles('admin'), CreateZone);
 router.get('/zone', protect, authorizeRoles('admin'), getZone);
 router.post('/company', protect, authorizeRoles('admin'), Createcompany);
 router.get('/company', getCompany);
+router.put('/zone/:id', protect, authorizeRoles('admin'), UpdateZone);
+router.delete('/zone/:id', protect, authorizeRoles('admin'), DeleteZone);
+router.put('/company/:id', protect, authorizeRoles('admin'), UpdateCompany);    
+router.delete('/company/:id', protect, authorizeRoles('admin'), DeleteCompany);
 
 
 export default router;
