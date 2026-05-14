c1622db3a6d342005a320e5efa5b0a49632819c4
prashantpagare44 <prashantpagarejd@gmail.com>
2026-05-14 11:48:30 +0530

    permit distributor to  create rep file admin route

--- Backend/router/adminroutes.js
+++ Backend/router/adminroutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/create-distributor', protect, authorizeRoles('admin'), Distributor);
 router.put('/update-distributor/:id', protect, authorizeRoles('admin'), updateDistributor);
 router.put('/suspend-distributor/:id', protect, authorizeRoles('admin'), suspendDistributor);
-router.post('/create-rep', protect, authorizeRoles('admin'), Rep);
+router.post('/create-rep', protect, authorizeRoles('admin','distributor'), Rep);
 router.get('/distributors', protect, authorizeRoles('admin'), getDistributor);
 router.get('/reps', protect, authorizeRoles('admin'), getReps);  
 
